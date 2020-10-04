63fd4ee7244c7374bca6ff7b706b48f1cd681bf4
Ayush Singh <56271985+ayushazuri@users.noreply.github.com>
2020-10-04 19:25:40 +0530

    Delete gitTest.java

--- gitTest.java
+++
@@ -1,7 +0,0 @@
-class gitTest{
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("In the new Branch");
-		// Inside new checking on github
-	}
-}
